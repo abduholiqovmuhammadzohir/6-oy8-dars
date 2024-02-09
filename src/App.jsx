e444b7cbcbd714f5e278bbe8776942b69d52753c
abduholiqovmuhammadzohir <abduholiqovmuhammadzohir@gmail.com>
2024-02-09 22:54:42 +0500

    Vazifa tugadi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useRef, useState, useEffect } from 'react'
+import detale from './assets/delete.png'
+import updateIcon from './assets/update.png'
+
 import './App.css'
 
 function App() {
   const inputRef = useRef('');
   const [dataLocal, setDataLocal] = useState([]);
   const [update, setUpdate] = useState(false)
-  const [updateId, setUpdateId] = useState();
-  const [updateName, setUpdateName] = useState("");
+  const [updateId, setUpdateId] = useState(0);
 
   useEffect(() => {
     let data = [];
@@ -70,13 +72,12 @@ function App() {
     }
   }
   function handleUpdateMain() {
-    handleUpdate();
-    inputRef.current.value = ""
-    setUpdate(false)
-
-  }
-
-  function handleUpdate() {
+    if (inputRef.current.value.trim().length < 3) {
+      alert("3 tadan kam belgin kiritish mumkin emas")
+      inputRef.current.focus();
+      return
+    }
+    setUpdate(!update)
     let copied = JSON.parse(JSON.stringify(dataLocal))
     copied = copied.map(el => {
       if (el.id == updateId) {
@@ -86,7 +87,10 @@ function App() {
     })
     setDataLocal(copied)
     localStorage.setItem('todos', JSON.stringify(copied))
+    inputRef.current.value = ""
   }
+
+
 
 
 
@@ -110,7 +114,7 @@ function App() {
           {
             update ? <button onClick={(e) => {
               e.preventDefault();
-              handleUpdateMain() 
+              handleUpdateMain();
             }}>Update</button> : <button onClick={handleSubmit}>OK</button>
           }
         </form>
@@ -127,12 +131,10 @@ function App() {
                   <span className='deeds'>
                     <span onClick={() => {
                       setUpdate(true)
-                      setUpdateName(todo.name)
                       setUpdateId(todo.id)
-                      inputRef.current.value = updateName
-                      handleUpdate()
-                    }} >update</span>
-                    <span onClick={() => { handleDelete(todo.id, todo.name) }}>delete</span>
+                      inputRef.current.value = todo.name
+                    }} ><img src={updateIcon} width={35} alt="" /></span>
+                    <span onClick={() => { handleDelete(todo.id, todo.name) }}> <img src={detale} width={35} alt="" /> </span>
                   </span>
                 </li>
               )
